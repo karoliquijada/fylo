b3320b28b2893ddf1cf9343806f99ae9690b5e2d
karoliquijada <kquijada9894@gmail.com>
2022-10-28 21:57:56 -0400

    header section

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './component/navbar/navbar.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ButtonComponent } from './component/button/button.component';
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    ButtonComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent,
+    ButtonComponent
   ]
 })
 export class SharedModule { }
